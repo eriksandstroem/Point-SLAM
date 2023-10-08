0eb970039a44b03d2f4e512836d81349c019c4df
Erik Sandström <erik.sandstrm@gmail.com>
2023-10-08 10:44:58 +0200

    Update download_tum.sh

--- scripts/download_tum.sh
+++ scripts/download_tum.sh
@@ -1,5 +1,5 @@
-mkdir -p /scratch_net/biwidl212/liyueli/datasets/TUM_RGBD
-cd /scratch_net/biwidl212/liyueli/datasets/TUM_RGBD
+mkdir -p datasets/TUM_RGBD
+cd datasets/TUM_RGBD
 wget https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
 tar -xvzf rgbd_dataset_freiburg1_desk.tgz
 wget https://cvg.cit.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk2.tgz
